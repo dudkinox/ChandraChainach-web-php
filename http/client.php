09ebebfa6077687ac636966a2f759e9b0f2ac476
ศุภณัฐ ชินราช <guitar@odds.team>
2022-02-25 03:58:50 +0700

    login success

---
+++ http/client.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "id18411732_dudkinox", 'JVC=2nURDddI1nL{', "id18411732_project");
+$conn->set_charset("utf8");
--- services/loginService.php
+++ services/loginService.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
+require('../http/client.php');
 
 $username = $_POST["username"];
-$password = $_POST["password"];
+$password = md5($_POST["password"]);
 
-if ($username == "adminaon" && $password == "admin") {
-    $_SESSION["type"] = "admin";
-    header("Location: /admin");
+$query = "SELECT * FROM `login` WHERE username = '$username' AND `password` = '$password'";
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+    $_SESSION["login"] = true;
 } else {
     $_SESSION["error"] = "1";
-    header("Location: ../?page=login");
 }
+
+header("Location: ../");
